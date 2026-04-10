24584e2a8511efd47c0eeb2cf509b151e3078789
nozwock <57829219+nozwock@users.noreply.github.com>
2026-04-11 00:28:33 +0530

    Add support for `ILHook` to `ManagedHooks` type

--- Common/Hooks.cs
+++ Common/Hooks.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using BepInEx.Logging;
+using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 
 namespace Common.Hooks;
@@ -10,61 +11,95 @@ namespace Common.Hooks;
 public class ManagedHooks(ManualLogSource logger) : IDisposable
 {
     readonly ManualLogSource logger = logger;
-    readonly Dictionary<Delegate, List<Hook>> hookLists = [];
+    readonly Dictionary<Delegate, List<IDetour>> detourLists = [];
 
     public void Dispose()
     {
-        foreach (var hooks in hookLists.Values)
-            hooks.ForEach(hook => hook.Dispose());
-        hookLists.Clear();
+        foreach (var detours in detourLists.Values)
+            detours.ForEach(detour => detour.Dispose());
+        detourLists.Clear();
     }
 
     public void Add(MethodBase method, Delegate to)
     {
-        if (hookLists.TryGetValue(to, out var hooks))
+        if (detourLists.TryGetValue(to, out var detours))
         {
-            var isDuplicate = hooks
-                .Any(hook => hook.Method == method && hook.Target == to.Method);
+            var isDuplicate = detours
+                .OfType<Hook>()
+                .Any(hook => hook.Method.Equals(method) && hook.Target.Equals(to.Method));
             if (!isDuplicate)
-                hooks.Append(new(method, to));
+                detours.Append(new Hook(method, to));
             else
                 logger.LogDebug(
-                    $"{nameof(ManagedHooks)}: Skipping duplicate hook: {GetHookString(method, to.Method)}");
+                    $"{nameof(ManagedHooks)}: Skipping duplicate Hook: {GetHookString(method, to.Method)}");
         }
         else
-            hookLists[to] = [new(method, to)];
+            detourLists[to] = [new Hook(method, to)];
     }
 
-    public (MethodBase, MethodBase, bool)[] GetAllHookedMethods()
-        => [.. hookLists.Values.SelectMany(it => it)
-            .Select(hook => (hook.Method, hook.Target, hook.IsApplied))];
+    public void Add(MethodBase method, ILContext.Manipulator manipulator)
+    {
+        if (detourLists.TryGetValue(manipulator, out var detours))
+        {
+            var isDuplicate = detours
+                .OfType<ILHook>()
+                .Any(hook => hook.Method.Equals(method));
+            if (!isDuplicate)
+                detours.Append(new ILHook(method, manipulator));
+            else
+                logger.LogDebug(
+                    $"{nameof(ManagedHooks)}: Skipping duplicate ILHook: {GetHookString(method, manipulator.Method)}");
+        }
+        else
+            detourLists[manipulator] = [new ILHook(method, manipulator)];
+    }
+
+    public IEnumerable<(MethodBase, MethodBase, bool)> GetAllPatchedMethods()
+        => GetAllHookedMethods().Concat(GetAllModifiedMethods());
+
+    public IEnumerable<(MethodBase, MethodBase, bool)> GetAllHookedMethods()
+        => detourLists.Values
+            .SelectMany(it => it)
+            .OfType<Hook>()
+            .Select(hook => (hook.Method, hook.Target, hook.IsApplied));
+
+    public IEnumerable<(MethodBase, MethodBase, bool)> GetAllModifiedMethods()
+        => detourLists
+            .SelectMany(
+                kvp => kvp.Value.OfType<ILHook>(),
+                (kvp, hook) => (kvp.Key, hook))
+            .Select(tuple =>
+            {
+                var (manipulator, hook) = tuple;
+                return (hook.Method, (MethodBase)manipulator.Method, hook.IsApplied);
+            });
 
     public void ApplyDelegate(Delegate hookDelegate)
     {
-        if (hookLists.TryGetValue(hookDelegate, out var hooks))
-            hooks.ForEach(hook => hook.Apply());
+        if (detourLists.TryGetValue(hookDelegate, out var detours))
+            detours.ForEach(detour => detour.Apply());
     }
 
     public void UndoDelegate(Delegate hookDelegate)
     {
-        if (hookLists.TryGetValue(hookDelegate, out var hooks))
-            hooks.ForEach(hook => hook.Undo());
+        if (detourLists.TryGetValue(hookDelegate, out var detours))
+            detours.ForEach(detour => detour.Undo());
     }
 
     public void DisposeDelegate(Delegate hookDelegate)
     {
-        if (hookLists.TryGetValue(hookDelegate, out var hooks))
+        if (detourLists.TryGetValue(hookDelegate, out var detours))
         {
-            hooks.ForEach(hook => hook.Dispose());
-            hookLists.Remove(hookDelegate);
+            detours.ForEach(detour => detour.Dispose());
+            detourLists.Remove(hookDelegate);
         }
     }
 
-    public void LogAllHookedMethods()
+    public void LogAllPatchedMethods()
     {
-        foreach (var (method, target, isActive) in GetAllHookedMethods())
+        foreach (var (method, target, isActive) in GetAllPatchedMethods())
         {
-            var msg = $"Hooked({isActive}): {GetHookString(method, target)}";
+            var msg = $"Patched({isActive}): {GetHookString(method, target)}";
             if (isActive)
                 logger.LogInfo(msg);
             else
